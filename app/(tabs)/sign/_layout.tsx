2a0b1ee67cf5842e1efa7de80fc6fe5d79c5a730
Sergiu <condratiuc.sergiu@gmail.com>
2025-09-01 19:50:24 +0200

    1

--- app/(tabs)/sign/_layout.tsx
+++ app/(tabs)/sign/_layout.tsx
@@ -1,12 +1,13 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 export default function SignStack() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Sign' }} options={{ headerShown: false }}/>
-      <Stack.Screen name="create" options={{ title: 'Documents', presentation: 'modal', }}/>
-      <Stack.Screen name="view_all" options={{ title: 'All Signatures' }} />
-      <Stack.Screen name="settings_sign" options={{ title: 'Sign Settings' }} />
+      <Stack.Screen name="index" options={{ title: "Sign", headerShown: false }} />
+      <Stack.Screen name="[id]" options={{ title: "Document Details" }} />
+      <Stack.Screen name="create" options={{ title: "Documents", presentation: "modal" }} />
+      <Stack.Screen name="view_all" options={{ title: "All Signatures" }} />
+      <Stack.Screen name="settings_sign" options={{ title: "Sign Settings" }} />
     </Stack>
   );
 }
